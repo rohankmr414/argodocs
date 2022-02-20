1f89cfd14f428d1e0c687a69c82338ad5c1ea3c8
Junaid Rahim <junaidrahim8d@gmail.com>
2022-02-20 21:41:57 +0530

    add workflow parsing

--- cmd/argodocs/main.go
+++ cmd/argodocs/main.go
@@ -1,1 +1,12 @@
-package argodocs
+package main
+
+import (
+	"github.com/junaidrahim/argodocs/workflow"
+)
+
+func main() {
+	_, err := workflow.ParseFiles("/tmp/workflows/*.yaml")
+	if err != nil {
+		return
+	}
+}
